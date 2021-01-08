0c00d22855e8f5c7112762c6ed9942b171227d85
Blake W. Ford <blakewford@gmail.com>
2021-01-07 23:32:03 -0600

    Add OR instruction

--- marcher-energy.js
+++ marcher-energy.js
@@ -25,6 +25,7 @@ var marcher = {"hardwareName":"Marcher","hardwareId":"e53fcc4d-a2c9-4833-9cc9-2e
     {"name":"MOVZX","joules":"0.000000004"},
     {"name":"MOVABS","joules":"0.000000003"},
     {"name":"POP","joules":"0.000000016"},
+    {"name":"OR","joules":"0.000000005"},
     {"name":"PUSH","joules":"0.000000016"},
     {"name":"RET","joules":"0.000000000"},
     {"name":"SETZ","joules":"0.000000002"},

e1eec7b8e94a07be39c18f70c081c2e15f42901d
Blake W. Ford <blakewford@gmail.com>
2021-01-26 18:41:55 -0600

    Update binary translation mechanism

--- synthetic.js
+++ synthetic.js
@@ -1,50 +1,70 @@
+var usedMap = null
 var synthMap = null
+function emit(ngram)
+{
+    var test = bigrams[ngram]
+    if(test == null) return ""
+
+    var key = Object.keys(test)[0]
+    if(usedMap.has(test))
+    {
+        var ndx = 0;
+        var value = test[key]
+        var current = usedMap.get(test)
+        while(current >= value && current < 100)
+        {
+            ++ndx;
+            if(ndx < Object.keys(test).length)
+            {
+                value += test[Object.keys(test)[ndx]]
+            }
+            else
+            {
+                ndx = 0
+                current = 0
+                break;
+            }
+        }
+        key = Object.keys(test)[ndx];
+        usedMap.set(test, current+1)
+    }
+    else
+    {
+        usedMap.set(test, 1)
+    }
+
+    return key
+}
 
 function buildSyntheticInstanceMap(selected)
 {
+    usedMap = new Map()
     synthMap = new Map()
     if(!selected.triple.includes("x86_64")) return
 
-    for(var i = 0; i < selected.run.length; i++)
+    for(var i = 0; i < selected.run.length-1; i+=2)
     {
-        var mnem = ""
-        var value = 0
         var first = selected.run[i].m.toUpperCase()
-        for(var j = 0; j < x64.instr.length; j++)
+        var second = selected.run[i+1].m.toUpperCase()
+        var ngram = emit(first + "-" + second)
+
+        if(ngram == "")
         {
-            if(first == x64.instr[j].toUpperCase())
+            ngram = "UNKNOWN-UNKNOWN"
+        }
+
+        var instructions = ngram.split("-");
+        for(var j = 0; j < instructions.length; j++)
+        {
+            var value = 0
+            var instruction = instructions[j];
+            if(synthMap.has(instruction))
             {
-                mnem = aarch64.instr[j].toUpperCase()
-                break
+                value = synthMap.get(instruction)
             }
-        }
 
-        /* DANGEROUS */
-        // x86-64 has multiple mnemonics for the same instruction
-        // Our internal dictionary is based on the short form
-        if(mnem == "")
-        {
-            for(var j = 0; j < x64.instr.length; j++)
-            {
-                if(first.slice(0,-1) == x64.instr[j].toUpperCase())
-                {
-                    mnem = aarch64.instr[j].toUpperCase()
-                    break
-                }
-            }
+            value++;
+            synthMap.set(instruction, value)
         }
-
-        if(mnem == "")
-        {
-//            console.log("Synthetic Match - Not Found " + first)
-        }
-
-        if(synthMap.has(mnem))
-        {
-            value = synthMap.get(mnem)
-        }
-
-        value++;
-        synthMap.set(mnem, value)
     }
 }
